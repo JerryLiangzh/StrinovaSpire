1e2b6470022f1f01eb1775efa26135ff43f966fd
JerryLiangzh <115389771+JerryLiangzh@users.noreply.github.com>
2026-03-21 13:43:29 +0800

    v0.2.1
    remove useless code, replace fake png within RIFFwebp head with true png

--- src/main/java/StrinovaMod/characters/Navigator.java
+++ src/main/java/StrinovaMod/characters/Navigator.java
@@ -1,16 +1,9 @@
 package StrinovaMod.characters;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Texture;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.blue.Zap;
@@ -20,7 +13,6 @@ import com.megacrit.cardcrawl.core.EnergyManager;
 import com.megacrit.cardcrawl.cutscenes.CutscenePanel;
 import com.megacrit.cardcrawl.events.city.Vampires;
 import com.megacrit.cardcrawl.helpers.FontHelper;
-import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.helpers.ScreenShake;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.localization.CharacterStrings;
@@ -35,7 +27,6 @@ import basemod.abstracts.CustomPlayer;
 public class Navigator extends CustomPlayer {
     public static final CharacterStrings characterStrings = CardCrawlGame.languagePack.getCharacterString(StrinovaMod.makeID("Navigator"));
     private static final String MY_CHARACTER_IMAGE = "StrinovaMod/images/characters/Navigator.png";
-    private static final String MY_CHARACTER_IMAGE_FALLBACK = "StrinovaMod/images/characters/NavigatorPortrait.png";
     private static final String MY_CHARACTER_SHOULDER_1 = "StrinovaMod/images/characters/shoulder1.png";
     private static final String MY_CHARACTER_SHOULDER_2 = "StrinovaMod/images/characters/shoulder2.png";
     private static final String CORPSE_IMAGE = "StrinovaMod/images/characters/corpse.png";
@@ -69,163 +60,6 @@ public class Navigator extends CustomPlayer {
                 0.0F, 0.0F, 200.0F, 220.0F,
                 new EnergyManager(3)
         );
-
-        // Base game render path directly dereferences this.img, so ensure it is loaded.
-        if (this.img == null) {
-            this.img = ImageMaster.loadImage(MY_CHARACTER_IMAGE);
-        }
-        if (this.img == null) {
-            this.img = loadTextureFromClasspath(MY_CHARACTER_IMAGE);
-        }
-        if (this.img == null) {
-            this.img = ImageMaster.loadImage(MY_CHARACTER_IMAGE_FALLBACK);
-        }
-        if (this.img == null) {
-            this.img = loadTextureFromClasspath(MY_CHARACTER_IMAGE_FALLBACK);
-        }
-        if (this.shoulderImg == null) {
-            this.shoulderImg = ImageMaster.loadImage(MY_CHARACTER_SHOULDER_1);
-        }
-        if (this.shoulderImg == null) {
-            this.shoulderImg = loadTextureFromClasspath(MY_CHARACTER_SHOULDER_1);
-        }
-        if (this.shoulder2Img == null) {
-            this.shoulder2Img = ImageMaster.loadImage(MY_CHARACTER_SHOULDER_2);
-        }
-        if (this.shoulder2Img == null) {
-            this.shoulder2Img = loadTextureFromClasspath(MY_CHARACTER_SHOULDER_2);
-        }
-        if (this.corpseImg == null) {
-            this.corpseImg = ImageMaster.loadImage(CORPSE_IMAGE);
-        }
-        if (this.corpseImg == null) {
-            this.corpseImg = loadTextureFromClasspath(CORPSE_IMAGE);
-        }
-        if (this.img == null) {
-            throw new IllegalStateException("Failed to load character image: " + MY_CHARACTER_IMAGE + " (fallback: " + MY_CHARACTER_IMAGE_FALLBACK + ") | " + getTextureLoadDiagnostics(MY_CHARACTER_IMAGE));
-        }
-    }
-
-    private static Texture loadTextureFromClasspath(String path) {
-        Texture fromInternal = loadTextureFromInternalFile(path);
-        if (fromInternal != null) {
-            return fromInternal;
-        }
-
-        InputStream inputStream = openResourceStream(path);
-        if (inputStream == null) {
-            return null;
-        }
-        try {
-            byte[] data = readAllBytes(inputStream);
-            Pixmap pixmap = new Pixmap(data, 0, data.length);
-            Texture texture = new Texture(pixmap);
-            pixmap.dispose();
-            return texture;
-        } catch (Exception ignored) {
-            return null;
-        } finally {
-            try {
-                inputStream.close();
-            } catch (IOException ignored) {
-            }
-        }
-    }
-
-    private static Texture loadTextureFromInternalFile(String path) {
-        try {
-            FileHandle handle = Gdx.files.internal(path);
-            if (handle != null && handle.exists()) {
-                return new Texture(handle);
-            }
-        } catch (Exception ignored) {
-        }
-        return null;
-    }
-
-    private static InputStream openResourceStream(String path) {
-        InputStream stream = Navigator.class.getResourceAsStream("/" + path);
-        if (stream != null) {
-            return stream;
-        }
-
-        ClassLoader navigatorLoader = Navigator.class.getClassLoader();
-        if (navigatorLoader != null) {
-            stream = navigatorLoader.getResourceAsStream(path);
-            if (stream != null) {
-                return stream;
-            }
-        }
-
-        ClassLoader contextLoader = Thread.currentThread().getContextClassLoader();
-        if (contextLoader != null) {
-            stream = contextLoader.getResourceAsStream(path);
-            if (stream != null) {
-                return stream;
-            }
-        }
-
-        return ClassLoader.getSystemResourceAsStream(path);
-    }
-
-    private static String getTextureLoadDiagnostics(String path) {
-        boolean internalExists = false;
-        boolean classResourceExists = false;
-        boolean navigatorLoaderExists = false;
-        boolean contextLoaderExists = false;
-        boolean systemLoaderExists = false;
-
-        try {
-            FileHandle handle = Gdx.files.internal(path);
-            internalExists = handle != null && handle.exists();
-        } catch (Exception ignored) {
-        }
-
-        try (InputStream ignored = Navigator.class.getResourceAsStream("/" + path)) {
-            classResourceExists = ignored != null;
-        } catch (Exception ignored) {
-        }
-
-        try {
-            ClassLoader navigatorLoader = Navigator.class.getClassLoader();
-            if (navigatorLoader != null) {
-                try (InputStream ignored = navigatorLoader.getResourceAsStream(path)) {
-                    navigatorLoaderExists = ignored != null;
-                }
-            }
-        } catch (Exception ignored) {
-        }
-
-        try {
-            ClassLoader contextLoader = Thread.currentThread().getContextClassLoader();
-            if (contextLoader != null) {
-                try (InputStream ignored = contextLoader.getResourceAsStream(path)) {
-                    contextLoaderExists = ignored != null;
-                }
-            }
-        } catch (Exception ignored) {
-        }
-
-        try (InputStream ignored = ClassLoader.getSystemResourceAsStream(path)) {
-            systemLoaderExists = ignored != null;
-        } catch (Exception ignored) {
-        }
-
-        return "internal=" + internalExists
-                + ", class=" + classResourceExists
-                + ", navLoader=" + navigatorLoaderExists
-                + ", ctxLoader=" + contextLoaderExists
-                + ", sysLoader=" + systemLoaderExists;
-    }
-
-    private static byte[] readAllBytes(InputStream inputStream) throws IOException {
-        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-        byte[] chunk = new byte[4096];
-        int read;
-        while ((read = inputStream.read(chunk)) != -1) {
-            buffer.write(chunk, 0, read);
-        }
-        return buffer.toByteArray();
     }
 
     public ArrayList<String> getStartingDeck() {
